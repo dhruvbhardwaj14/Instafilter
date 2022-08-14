8f19ba461c04fb868d0c189533be962fbf0d5a7e
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2022-08-14 11:23:41 +0530

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Dhruv Bhardwaj on 14/08/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
